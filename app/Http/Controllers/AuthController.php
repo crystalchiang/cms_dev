feb2e3a619dc65526fc82d2c6aab29dc33da2c21
CrystalChiang <crystal@jigentec.com>
2020-11-27 18:44:59 +0800

    1. 註冊加入權限欄位 2. 新增權限資料表及超級管理員資料 3.預設新增超級管理員、管理員、會員身份

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|confirmed|min:6',
+            'password' => 'required|string|confirmed|min:6'
         ]);
 
         if($validator->fails()){
@@ -63,7 +63,7 @@ class AuthController extends Controller
 
         $user = User::create(array_merge(
                     $validator->validated(),
-                    ['password' => bcrypt($request->password)]
+                    ['password' => bcrypt($request->password), 'role_id' => 3]
                 ));
 
         return response()->json([
@@ -114,7 +114,7 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => auth()->user(),
         ]);
     }
 
